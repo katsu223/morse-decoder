dc75933c29357dc75828e7d90357329bfcbc020a
Eduard Ten <ten.edik220302@gmail.com>
2019-10-06 13:52:54 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,31 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let lengthOfExpr = expr.length;
+    const numOfLetters = lengthOfExpr / 10;
+    let decodedString = '';
+    startSubstr = 0;
+    
+    for (let i = 1; i <= numOfLetters; i++, startSubstr += 10) {
+        let subOfCodedString = expr.substr(startSubstr, 10);
+
+        if (subOfCodedString[0] === '*') {
+            decodedString += ' '; 
+            continue;
+        }
+
+        let symbolOfMorseTable = ''
+        let j = 0;
+        while (subOfCodedString[j] === '0') j++;
+
+        for (j++; j < 10; j += 2) {
+            (subOfCodedString[j] === '0') ? symbolOfMorseTable += '.' : symbolOfMorseTable += '-';
+        }
+
+        decodedString += MORSE_TABLE[symbolOfMorseTable];
+    }
+
+    return decodedString;
 }
 
 module.exports = {
